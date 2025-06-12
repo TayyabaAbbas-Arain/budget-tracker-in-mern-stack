bc01e98459e76620f5be3a0f052b1860b8784b4c
Tayyaba Abbas Arain <araintayyabaabbas@gmail.com>
2025-06-12 16:40:45 +0500

    Setup React Router and routed all pages in App.js

--- budget-tracker-client/src/App.js
+++ budget-tracker-client/src/App.js
@@ -1,13 +1,24 @@
-//import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
