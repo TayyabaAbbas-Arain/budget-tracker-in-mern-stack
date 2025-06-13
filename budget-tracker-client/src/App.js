2b85386a10122ea8a5d28ee39f7f5dd392253de7
Tayyaba Abbas Arain <araintayyabaabbas@gmail.com>
2025-06-13 11:32:20 +0500

    Added and tested NotFound route to handle undefined pages

--- budget-tracker-client/src/App.js
+++ budget-tracker-client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -16,7 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} /> {/* ✅ Important */}
       </Routes>
     </Router>
   );
